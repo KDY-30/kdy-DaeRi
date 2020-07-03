c821af582fae296767b5b1b55b0d6c811870dba2
김다연 <ekfqlc2193@gmail.com>
2020-07-03 12:35:58 +0900

    11시 스터디 작업 내역

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/views/HelloWorld'
+import Call from '@/views/Call'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/Call',
+      name: 'Call',
+      component: Call
     }
   ]
 })
